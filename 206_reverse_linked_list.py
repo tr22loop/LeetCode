0b937cad0c569bec03024cf0a70d6fe9e5a6b467
tr22loop <65411605+tr22loop@users.noreply.github.com>
2025-04-22 11:09:56 +0600

    Add 206_reverse_linked_list.py - LeetCode Problem 206

---
+++ 206_reverse_linked_list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        if head.next == None:
+            return head
+        
+        prev = None
+        curr = head
+        while ( curr != None):
+            newNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = newNode
+        head = prev
+        return head
